a3bedf2622aece6c084602c8c138785c088ec4af
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-26 14:29:28 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,24 @@
+import java.lang.Exception
+import java.lang.RuntimeException
+
 fun main(){
     var array = arrayOf(1,2,3,4,5,6,7,8,9,10)
 
     try {
-        println(array[9])
+        println(array[10])
     }catch(e: ArrayIndexOutOfBoundsException){
-        println("Został przechwycony wyjątek...")
+        println("Macierz nie zawiera elemntu o indeksie ${e.message}")
     }
 
     println("Dalszy ciąg programu...")
+
+    println(f0(110.5))
+}
+
+fun f0(x: Double):Double{
+    //chcielibyśmy żeby argument należał od -1 do 1
+    if(x<-1||x>1){
+        throw Exception("")
+    }
+    return x
 }
