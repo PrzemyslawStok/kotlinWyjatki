227e1d2bd135f564de1f7acd00e0956ab79efe96
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-26 14:17:53 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
+    var array = arrayOf(1,2,3,4,5,6,7,8,9,10)
 
+    try {
+        println(array[9])
+    }catch(e: ArrayIndexOutOfBoundsException){
+        println("Został przechwycony wyjątek...")
+    }
+
+    println("Dalszy ciąg programu...")
 }
